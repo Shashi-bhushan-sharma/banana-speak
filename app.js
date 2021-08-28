863276b40cb18d473b6a956cb4f889eb8177ae8c
AdarshDhir <dhiradarsh0789@gmail.com>
2021-08-28 22:09:36 +0530

    Input-Output and Using API

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput= document.querySelector("#txt-input");
+var txtOutput= document.querySelector("#txt-output");
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL ="https://api.funtranslations.com/translate/dothraki.json";
+
 console.log(txtInput)
 
+//  txtOutput.innerText="hello";
+ console.log(txtOutput);
+
+ function getTranslateURL(text){
+   return serverURL + "?" + "text=" + text
+ }
+
+ function ErrorHandler(error){
+   console.log("An Error Occured !")
+   alert("Something Wrong With the Server! \n Please Try Later.")
+ }
+
 function clickEventHandler()
-{ console.log("clicked!!")
-  console.log("Input",txtInput.value)
+{
+  //  console.log("clicked!!")
+  // console.log("Input",txtInput.value)
+  // txtOutput.innerText="dsvnsvdvd"+ txtInput.value+" hehehe";
+    fetch(getTranslateURL(txtInput.value))
+    .then(response => response.json())
+    .then(json => {
+      console.log(json.contents.translated)
+      txtOutput.innerText=json.contents.translated;})
+
+    .catch(ErrorHandler)
+  
 }
 
 btnTranslate.addEventListener("click",clickEventHandler)
 
-console.log(btnTranslate)
+
+
+// "input[name='translator']"
